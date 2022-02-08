41799d0d063b0e6fc0e4dbb32ea00f50fddb9ab7
Zanco1993 <zanconatofabio@gmail.com>
2022-02-08 12:13:25 +0100

    header up ok

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -3,19 +3,32 @@
         <div class="content-info">
             <div class="container contacts">
                 <div class="open-hours">
-
+                    <i class="fas fa-clock"></i>
+                    <span> Open Hours: Mon - Sat - 9:00 - 18:00</span>
                 </div>
                 <div class="info-social">
-                    
+                    <ul>
+                        <li>
+                            <i class="fas fa-phone"></i>
+                            <span> +1 (305) 1234-5678</span>
+                        </li>
+                        <li>
+                            <i class="fas fa-envelope"></i>
+                            <span> hello@example.com</span>
+                        </li>
+                        <li><i class="fab fa-facebook-f"></i></li>
+                        <li><i class="fab fa-twitter"></i></li>
+                        <li><i class="fab fa-linkedin-in"></i></li>
+                    </ul>
                 </div>
             </div>
         </div>
 
-        <jumbotron>
+        <!-- <jumbotron>
             <nav>
 
             </nav>
-        </jumbotron>
+        </jumbotron> -->
 
 
     </header>
