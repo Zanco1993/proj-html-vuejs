c44b0dc98b5b16ad9e6597569a6f338ef4a9def6
Zanco1993 <zanconatofabio@gmail.com>
2022-02-09 16:17:58 +0100

    footer complete

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,15 +1,135 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <footer>
+      <div class="container box-card">
+        <div class="card">
+          <p class="py-4">N E X G E N</p>
+          <p class="py-4">
+            A functional HTML Template for Corporate & Business
+          </p>
+
+          <div class="generic-info">
+            <i class="fas fa-phone"></i>
+            <span> +1 (305) 1234-5678</span>
+          </div>
+          <div class="generic-info">
+            <i class="fas fa-envelope"></i>
+            <span>hello@example.com</span>
+          </div>
+          <div class="generic-info">
+            <i class="fas fa-map-marker-alt"></i>
+            <span>Main Avenue, 987</span>
+          </div>
+          <div class="generic-info">
+            <span class="buttons button-transparent">get in touch</span>
+          </div>
+        </div>
+
+        <div class="card about">
+          <h1 class="py-4">About</h1>
+          <ul>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>The company</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Institutional</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Social & Events</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Innovation</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Environment</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Technology</span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="card transport">
+          <h1 class="py-4">Transport</h1>
+          <ul>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Industrialized</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Chemicals</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Packaged Liquids</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Construction</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Laminated Wood</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>And others</span>
+            </li>
+          </ul>
+        </div>
+
+        <div class="card support">
+          <h1 class="py-4">Support</h1>
+          <ul>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Responsability</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Terms of Use</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>About Cookies</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Privacy</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Accessability</span>
+            </li>
+            <li>
+              <i class="fas fa-chevron-right"></i>
+              <span>Information</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="credits">
+        <div class="container policy">
+          <p>Enjoy the low  price. We are tracking any intention of privacy</p>
+          <p><i class="far fa-copyright"></i> 2020 NEXGEN in Proudly Powered by <span>Coding</span>.</p>
+        </div>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
+@import "../style/footer.scss";
 </style>
