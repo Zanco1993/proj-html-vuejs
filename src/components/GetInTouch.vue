4ae462d4d78415ce9a56ea14694cf2f8a9087714
Zanco1993 <zanconatofabio@gmail.com>
2022-02-09 12:10:33 +0100

    wp input text

--- src/components/GetInTouch.vue
+++ src/components/GetInTouch.vue
@@ -1,13 +1,40 @@
 <template>
-    <div></div>
+  <div class="container">
+    <div class="get-in-touch">
+      <div class="info-user">
+        <p class="font-mini-title"><strong>send a message</strong></p>
+        <div class="title">
+          <span class="font-title">Get in</span>
+          <span class="bg-title font-title">Touch</span>
+        </div>
+        <div class="description-service">
+          <p class="info-service">
+            We will respond to your message as soon as possible.
+          </p>
+        </div>
+
+        <div class="text-area">
+            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Name">
+            <input type="text" placeholder="Name">
+            
+        </div>
+      </div>
+
+      <div class="contact">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam vel in
+        laboriosam voluptatibus fuga, unde optio, dolor explicabo ipsa
+        recusandae sequi quis. Quasi harum fugiat quas a reiciendis totam ut.
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
+@import "../style/getInTouch.scss";
 </style>
